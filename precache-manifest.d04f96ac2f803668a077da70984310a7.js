443c232112aef780b161038e16cb7ad786cab77f
jrejoire <jrejoire@hotmail.fr>
2020-08-16 13:24:44 +0200

    Updates

---
+++ precache-manifest.d04f96ac2f803668a077da70984310a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435eb3dcad07069c5a5bf13586d11ba8",
+    "url": "/insys-game/index.html"
+  },
+  {
+    "revision": "606667c933c1e0193a5b",
+    "url": "/insys-game/static/css/main.8181e96e.chunk.css"
+  },
+  {
+    "revision": "a109cb19735dc96bedcd",
+    "url": "/insys-game/static/js/2.3c8f94b1.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/insys-game/static/js/2.3c8f94b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606667c933c1e0193a5b",
+    "url": "/insys-game/static/js/main.d3327d96.chunk.js"
+  },
+  {
+    "revision": "a82635a4398cff9adc7f",
+    "url": "/insys-game/static/js/runtime-main.b666fe7f.js"
+  }
+]);

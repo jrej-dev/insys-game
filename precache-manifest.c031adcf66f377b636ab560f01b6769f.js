4e8820474517184ac44c155efa13fd6365180e92
jrejoire <jrejoire@hotmail.fr>
2020-08-19 00:12:01 +0200

    Updates

---
+++ precache-manifest.c031adcf66f377b636ab560f01b6769f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a24651e625105df57162247881ec3e7",
+    "url": "/insys-game/index.html"
+  },
+  {
+    "revision": "702775546f40d2ce29ef",
+    "url": "/insys-game/static/css/main.8181e96e.chunk.css"
+  },
+  {
+    "revision": "99ef127b7c13eb26b83a",
+    "url": "/insys-game/static/js/2.432514a7.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/insys-game/static/js/2.432514a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "702775546f40d2ce29ef",
+    "url": "/insys-game/static/js/main.a5f450f3.chunk.js"
+  },
+  {
+    "revision": "a82635a4398cff9adc7f",
+    "url": "/insys-game/static/js/runtime-main.b666fe7f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0f209522c9ee6ed9a09ed44c7ce4f3a.js"
+  "/insys-game/precache-manifest.c031adcf66f377b636ab560f01b6769f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/insys-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

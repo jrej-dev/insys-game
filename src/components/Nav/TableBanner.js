c8f9645751979f5776716a7157b75a8429cafeab
Jrej <33304306+Jrejoire@users.noreply.github.com>
2020-08-31 11:19:38 +0200

    Update TableBanner.js

--- src/components/Nav/TableBanner.js
+++ src/components/Nav/TableBanner.js
@@ -1,20 +1,25 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-
-import { useObserver } from 'mobx-react';
+import React from 'react';
 import { toJS } from 'mobx';
+import { useObserver } from 'mobx-react';
+import { Link } from "react-router-dom";
 import StoreContext from '../../store/AppStore';
 
 const TableBanner = () => {
     const store = React.useContext(StoreContext);
-
-    return (
-      <div class="alert-banner w-full fixed top-0">
-        <label class="flex items-center justify-between w-full p-2 bg-red-500 shadow text-white">
-          You have created a table. You'll be redirected to the game once another player has joined.
-        </label>
-      </div>
-    )
+     return useObserver(() => {
+        if (toJS(store.userDetail) && toJS(store.userDetail).name && toJS(store.userTable)) {
+            return (
+              <div class="alert-banner w-full fixed top-0">
+                <label class="flex items-center justify-between w-full p-2 bg-red-500 shadow text-white">
+                  You have created a table. You'll be redirected to the game once another player has joined.
+                </label>
+              </div>
+            )
+        } else {
+            return <></>
+        }
+     })
+                        
 };
 
 export default TableBanner;

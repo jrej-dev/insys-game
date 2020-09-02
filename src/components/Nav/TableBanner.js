3b0ccaaa8e1742ac00b502dc6e4b5e9e3857a4f1
Jrej <33304306+Jrejoire@users.noreply.github.com>
2020-09-02 11:08:43 +0200

    Update TableBanner.js

--- src/components/Nav/TableBanner.js
+++ src/components/Nav/TableBanner.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { toJS } from 'mobx';
 import { useObserver } from 'mobx-react';
+import { useHistory } from "react-router-dom";
 //import { Link } from "react-router-dom";
 import StoreContext from '../../store/AppStore';
 
 const TableBanner = () => {
     const store = React.useContext(StoreContext);
+    const history = useHistory();
      return useObserver(() => {
-        if (toJS(store.userDetail) && toJS(store.userDetail).name && toJS(store.userTable)) {
+        if (toJS(store.userDetail) && toJS(store.userDetail).name && toJS(store.userTable) && !toJS(store.userTable).isFull) {
             return (
               <div className="alert-banner w-full">
                 <label className="items-center justify-center w-full p-2 bg-teal-400 shadow text-white hidden md:flex">
@@ -18,6 +20,14 @@ const TableBanner = () => {
                 </label>
               </div>
             )
+        } else if (toJS(store.userDetail) && toJS(store.userDetail).name && toJS(store.userTable) && toJS(store.userTable).isFull && !window.location.pathname.includes(toJS(store.userTable)._id)) {
+            return (
+                  <div className="alert-banner w-full" onClick={() => history.push(toJS(store.userTable).gameUrl)}>
+                    <label className="cursor-pointer items-center justify-center w-full p-2 bg-teal-400 shadow text-white hidden md:flex">
+                      You are attended to an on-going game. Click to be redirected to it...
+                    </label>
+                  </div>
+                )
         } else {
             return <></>
         }

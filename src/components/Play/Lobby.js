0377489091b1d768b1ce9919eaf34da0c76a5813
jrejoire <jrejoire@hotmail.fr>
2020-09-16 23:32:31 +0200

    minor fix

--- src/components/Play/Lobby.js
+++ src/components/Play/Lobby.js
@@ -241,15 +241,15 @@ const Lobby = () => {
                 </div>
             </div>
             <div id="warning" className="hidden h-screen/2 flex justify-center items-center my-10">
-                <div className="border border-gray-900 bg-gray-600 rounded-lg m-10 p-6 max-w-lg h-auto flex flex-col items-center">
+                <div className="border border-gray-900 bg-gray-600 rounded-lg m-10 p-10 sm:p-12 max-w-lg h-auto flex flex-col items-center">
                     <div className="flex flex-col items-center justify-center m-2">
                         <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
                         </svg>
                         <h1 className="font-bold">Warning</h1>
                     </div>
-                    <p className="text-justify my-2">Miniaturena is currently not optimized for touch screens and would affect your gaming experience.</p>
-                    <p className="text-justify my-2 font-bold">Please use a cursor-friendly device such as a laptop or PC.</p>
+                    <p className="text-left text-sm sm:text-md my-2">Miniaturena is currently not optimized for touch screens and they would affect your gaming experience.</p>
+                    <p className="text-left text-sm sm:text-md my-2 font-bold">Please use a cursor-friendly device such as a laptop or PC.</p>
                 </div>
             </div>
         </div>
